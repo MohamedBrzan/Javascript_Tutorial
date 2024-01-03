906773c7d997c7167b8aca66062836dddbc3d7c8
MohamedMahmoud <83614946+MohamedBrzan@users.noreply.github.com>
2024-01-03 19:44:33 +0200

    explained variables in js

--- Intro.js
+++ Intro.js
@@ -1,10 +1,5 @@
-//* Variables ( var - let - const )
-
-let x = 22;
-let y = 55;
-
-const z = x + y;
-
-z = 88;
-
-console.log(z);
+/* 
+syntax
+statements
+comments 
+*/
---
+++ variables.js
@@ -0,0 +1,30 @@
+//* variables
+
+//*  let - const
+
+//* let ( block scope )
+
+// if (true) {
+//   let x = 11;
+//   console.log(x, 'from if statement');
+// }
+
+// console.log(x, 'outside if statement');
+
+// let data = 5;
+
+// console.log(data);
+
+// data = 88;
+
+// console.log(data);
+
+//* const ( block scope )
+
+if (true) {
+  const data = 55;
+
+  console.log(data, 'from if statement');
+}
+
+console.log(data, 'outside if statement');
