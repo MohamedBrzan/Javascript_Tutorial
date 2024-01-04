f4e0d52373998afceb7943429042a6317639496f
MohamedMahmoud <83614946+MohamedBrzan@users.noreply.github.com>
2024-01-04 12:43:39 +0200

    finished type conversion.

---
+++ null_undefined.js
@@ -0,0 +1,9 @@
+//* null
+//* undefined
+
+
+let value = null;
+
+let str;
+
+console.log(typeof null)
---
+++ type_conversion.js
@@ -0,0 +1,25 @@
+//TODO: type conversion
+
+//* string conversion ( String )
+//* numeric conversion ( Number )
+//* boolean conversion ( boolean )
+
+let str = '11hth';
+// console.log(typeof str)
+
+// let y = Number(str);
+
+// console.log(y);
+
+// console.log(typeof str); // string
+// console.log(typeof y); // number
+// let z = String(y);
+// console.log(typeof z); // string
+
+// let bool = Boolean(1);
+
+// console.log(bool)
+
+let num = Number(true);
+
+console.log(num);
