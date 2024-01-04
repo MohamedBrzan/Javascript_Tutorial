4e037a6849b08b12255e854c67805a1dd6988f40
MohamedMahmoud <83614946+MohamedBrzan@users.noreply.github.com>
2024-01-04 16:00:31 +0200

    finished if statement

---
+++ if_statement.js
@@ -0,0 +1,27 @@
+/*
+if
+else
+else if
+*/
+
+// let x = 222;
+
+// if (x == 11) {
+//   console.log('No x not equal 11');
+// } else if (x == 11) {
+//   console.log('x equal 11 from else if');
+// } else {
+//   console.log('nothing right above');
+// }
+
+//* new if statement
+
+// x == 55
+//   ? console.log('yes it is equal 55')
+//   : x == 11
+//   ? console.log('yes it is equal 11')
+//   : x == 22
+//   ? console.log('yes it is equal 22')
+//   : console.log('this is else result');
+
+let y = 122;
