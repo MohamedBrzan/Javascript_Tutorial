893de932eab98fe0b36c5ddf086739035bafcdfc
MohamedMahmoud <83614946+MohamedBrzan@users.noreply.github.com>
2024-01-04 16:18:25 +0200

    finished looping

---
+++ loop.js
@@ -0,0 +1,17 @@
+//* while - do-while - for
+
+let i = 0;
+
+// while (i <= 20) {
+//   console.log(i);
+//   i++;
+// }
+
+// do {
+//   console.log(i);
+//   i++;
+// } while (i > 20);
+
+for (let x = 0; x <= 10; x++) {
+  console.log(x);
+}
