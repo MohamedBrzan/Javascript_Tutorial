5d7c0745027c305b0098b9cc6073ea15743d1495
MohamedBrzan <mohamedbrzan11@gmail.com>
2024-01-13 22:09:31 +0200

    teach function

---
+++ function.js
@@ -0,0 +1,40 @@
+//* function declaration
+
+let x = 50;
+let y = 33;
+
+let w = 22;
+let g = 34;
+
+let u = 23;
+let o = 11;
+
+// const num1 = 2;
+// const num2 = 2;
+
+
+function calculate() {
+ const num1 = 4;
+ const num2 = 44;
+ return num1 * num2;
+}
+
+console.log(num1);
+
+console.log(calculate());
+
+//* function expressions
+
+// const calculate = function (num1, num2) {
+//   return num1 * num2;
+// };
+
+// const factorial = function fac(num) {
+//   return num > 5 ? 1 : num * fac(num - 1);
+// };
+
+// const test = function () {
+//   console.log('this is message from test function');
+// };
+
+// test()
