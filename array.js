d8fd909fb5d1149b9e7f1bf7dfc51b66f0170745
MohamedBrzan <mohamedbrzan11@gmail.com>
2024-02-03 19:04:30 +0200

    reviewed some methods in array

--- array.js
+++ array.js
@@ -1,6 +1,9 @@
 let data = [45, 77, 200, 88, 200];
 
-const findResult = data.findLastIndex((value) => value >= 400);
+const forEachResult = data.forEach((ele, index, arr) => {
+  if (ele < 100) {
+    console.log(index, ele, arr);
+  }
+});
 
-console.log(findResult);
- 
+console.log(forEachResult);
