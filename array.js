70ea6f762162b8cbb236723765aa87b7971375eb
MohamedBrzan <mohamedbrzan11@gmail.com>
2024-02-10 23:48:49 +0200

    working in array for youtube channel

--- array.js
+++
@@ -1,9 +0,0 @@
-let data = [45, 77, 200, 88, 200];
-
-const forEachResult = data.forEach((ele, index, arr) => {
-  if (ele < 100) {
-    console.log(index, ele, arr);
-  }
-});
-
-console.log(forEachResult);
